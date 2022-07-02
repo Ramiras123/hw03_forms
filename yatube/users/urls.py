58cd29088be2bab9a072a6c03ddef040de514ed9
Aleksandr <smirnow-2012@mail.ru>
2022-07-02 17:49:57 +0300

    done project

--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -1,6 +1,8 @@
-from django.contrib.auth.views import LogoutView, LoginView, PasswordChangeDoneView
+from django.contrib.auth.views import LogoutView, LoginView
 from django.contrib.auth.views import PasswordChangeView, PasswordResetView
-from django.contrib.auth.views import PasswordResetDoneView, PasswordResetConfirmView
+from django.contrib.auth.views import PasswordChangeDoneView
+from django.contrib.auth.views import PasswordResetDoneView
+from django.contrib.auth.views import PasswordResetConfirmView
 from django.contrib.auth.views import PasswordResetCompleteView
 from django.urls import path
 from . import views
